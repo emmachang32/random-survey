dbdfd5cc7be715a9e59d66f92cab0742545d68a3
emmachang32 <65701742+emmachang32@users.noreply.github.com>
2022-12-11 19:52:31 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,8 +8,8 @@
 
 
 const urls = [
-	'https://www.surveycake.com/s/GPqmQ',
 	'https://www.surveycake.com/s/DPR8Y',
-	'https://www.surveycake.com/s/1O8X7',
-	'https://www.surveycake.com/s/6y1Yy'
+	'https://www.surveycake.com/s/GPqmQ',
+	'https://www.surveycake.com/s/6y1Yy',
+	'https://www.surveycake.com/s/1O8X7'
 ];
